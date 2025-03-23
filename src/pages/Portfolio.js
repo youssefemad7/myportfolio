1332d532c5f2f0cf293c116651f6eba355ed49e4
youssefemad7 <youssefabdelnaby12123@gmail.com>
2025-03-24 01:51:51 +0200

    finmal

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -236,7 +236,7 @@ const Portfolio = () => {
                 <a href="https://github.com/youssefemad7/Facebook-Clone">
                   <FontAwesomeIcon icon={faGithub} className="iconport" />
                 </a>
-                <a href="https://facebook-clone121232.vercel.app/">
+                <a href="https://facbook-clone12123.netlify.app/">
                   <FontAwesomeIcon icon={faLink} className="iconport" />
                 </a>
               </div>
