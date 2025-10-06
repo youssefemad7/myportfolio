3ee49f3e32e93f618b4d2ebd839a88c02640ab77
youssefemad7 <youssefabdelnaby12123@gmail.com>
2025-10-06 14:39:39 +0300

    age

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,59 +1,63 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 // import images
-import YoussefImg from '../img/home/youssef.jpg';
+import YoussefImg from "../img/home/youssef.jpg";
 
 // import link
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 // import motion
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 // import transition
-import { transition1 } from '../transitions';
+import { transition1 } from "../transitions";
 // import cursor context
-import { CursorContext } from '../context/CursorContext';
+import { CursorContext } from "../context/CursorContext";
 
 const About = () => {
   const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
   return (
     <motion.section
-      initial={{ opacity: 0, y: '100%' }}
+      initial={{ opacity: 0, y: "100%" }}
       animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: '100%' }}
+      exit={{ opacity: 0, y: "100%" }}
       transition={transition1}
-      className='section'
+      className="section"
     >
       <div
         onMouseEnter={mouseEnterHandler}
         onMouseLeave={mouseLeaveHandler}
-        className='container mx-auto h-full relative'
+        className="container mx-auto h-full relative"
       >
         {/* text & img wrapper */}
-        <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16'>
+        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
           {/* image */}
-          <div className='flex-1 max-h-96 lg:max-h-max order-2 lg:order-none '>
-
-            <img src={YoussefImg} alt=''      
-            className='img1'
-            style={{height:'600px'}}/>
+          <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none ">
+            <img
+              src={YoussefImg}
+              alt=""
+              className="img1"
+              style={{ height: "600px" }}
+            />
           </div>
           {/* text */}
           <motion.div
-            initial={{ opacity: 0, y: '-80%' }}
+            initial={{ opacity: 0, y: "-80%" }}
             animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: '-80%' }}
+            exit={{ opacity: 0, y: "-80%" }}
             transition={transition1}
-            className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start'
+            className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start"
           >
-            <h1 className='h1'>About me</h1>
-            <p className='mb-12 max-w-sm'>
-              My Name Is Youssef Emad Abdelnaby
-            Iam 20 years Live in cairo. 
-              <b>  Iam Software Engineering , Front End Develober And Freelancer</b>
+            <h1 className="h1">About me</h1>
+            <p className="mb-12 max-w-sm">
+              My Name Is Youssef Emad Abdelnaby Iam 23 years Live in cairo.
+              <b>
+                {" "}
+                Iam Software Engineering , Front End Develober And Freelancer
+              </b>
               <br />
               <br />
-             If you want to make a Website just send message for me.
-             And you can see my works to see what can i do
+              If you want to make a Website just send message for me. And you
+              can see my works to see what can i do
             </p>
-            <Link to={'/portfolio'} className='btn'>
+            <Link to={"/portfolio"} className="btn">
               View my work
             </Link>
           </motion.div>
